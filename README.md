a7805b0c87d05c31f73cd460d325a773c90a17f9
Clue Mediator <cluemediator@gmail.com>
2022-04-28 16:08:57 +0530

    Implement demo

---
+++ README.md
@@ -0,0 +1,30 @@
+# react-add-fonts
+
+How to add fonts to the React application
+
+## Documentation
+
+[https://www.cluemediator.com/how-to-add-fonts-to-the-react-application](https://www.cluemediator.com/how-to-add-fonts-to-the-react-application)
+
+## Quick Start
+
+Follow the below steps to run the project.
+
+1. Clone repository
+2. Run `npm i` command to install dependencies
+3. Execute `npm start` command to run the project
+
+## Output
+
+[![React Example](https://www.cluemediator.com/wp-content/uploads/2022/04/output-how-to-add-fonts-to-the-react-application-clue-mediator.jpg)](https://www.cluemediator.com/how-to-add-fonts-to-the-react-application)
+
+**Follow us on [GitHub](https://github.com/cluemediator) for more update.**
+
+## Connect with us
+
+Website: [Clue Mediator](https://www.cluemediator.com)  
+Like us on [Facebook](https://www.facebook.com/thecluemediator)  
+Follow us on [Twitter](https://twitter.com/cluemediator)  
+Join us on [Telegram](https://t.me/cluemediator)  
+Subscribe us on [YouTube](https://www.youtube.com/ClueMediator)  
+Follow us on [Instagram](https://www.instagram.com/clue_mediator)
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h2>Add fonts to the React app - <a href="https://www.cluemediator.com" target="_blank" rel="noreferrer noopener">Clue Mediator</a></h2>
+
+      <div className="local-font">
+        <strong>Font Family: OpenSans</strong><br />
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </div>
+
+      <div className="url-font">
+        <strong>Font Family: Changa</strong><br />
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </div>
+
+      <div className="css-font">
+        <strong>Font Family: Updock</strong><br />
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </div>
+    </div>
+  );
+}
+
+export default App;
